fe83b34cbf9e427fe502e737cf1f389ac3302453
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-06-04 16:36:24 +0300

    Fix the extension to be the current branch. Not an arg.

--- run_coconut_dev.sh
+++ run_coconut_dev.sh
@@ -1,5 +1,7 @@
-docker kill coconut-devenv-$2
-docker rm coconut-dev-$2
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo $BRANCH
+docker kill coconut-devenv-$BRANCH
+docker rm coconut-dev-$BRANCH
 xhost local:root
-docker run --name coconut-devenv-$2 -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/coconut -p 8080:6000 zafeirakopoulos/coconut:devenv-$2
+docker run --name coconut-devenv-$BRANCH -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/coconut -p 8080:6000 zafeirakopoulos/coconut:devenv-$BRANCH
 
